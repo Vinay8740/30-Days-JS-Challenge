193ebec38d234ce606b0a3ae42e69ec119ae95dd
Vinay8740 <118375035+Vinay8740@users.noreply.github.com>
2024-07-13 21:38:10 +0530

    This is Day 1 of 30 days JS challenge

---
+++ Day 1/Day1.js
@@ -0,0 +1,47 @@
+// Task 1 
+var demo1 = 2;
+console.log(demo1);
+
+// Task 2
+let demo2 = 'Hitesh';
+console.log(demo2);
+
+// Task 3
+const demo3 = true;
+console.log(demo3);
+
+// Task 4
+let var1 = 3;
+console.log(typeof(var1));
+
+let var2 = 'Javascript';
+console.log(typeof(var2));
+
+let var3 = true;
+console.log(typeof(var3));
+
+let var4 = {
+    CourseName :'30 Days JavaScript Challenge',
+    Mentor : 'Hitesh',
+    isitfree : true
+}
+console.log(typeof(var4));
+
+let var5 =['JS',30,true]
+console.log(typeof(var5));
+
+// Task 5
+let value1 = 5; // let having only local scope
+console.log(value1);
+
+value1 = 10;
+console.log(value1);
+
+// Task 6
+const value2 = 50;;
+console.log(value2);
+
+value2 = 60;
+console.log(value1); // Uncaught TypeError TypeError: Assignment to constant variable.
+
+
