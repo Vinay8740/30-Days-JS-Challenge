130a4565913de02a7121a2b4d2a096001b018857
Vinay8740 <118375035+Vinay8740@users.noreply.github.com>
2024-07-14 21:13:01 +0530

    Day 2 Completed

---
+++ Day 2 Operators/Day 2 Operators.js
@@ -0,0 +1,55 @@
+let num1 = 20;
+let num2 = 10;
+// Task 1: Add two Numbers
+console.log(num1+num2);
+
+// Task 2: Subtract two Numbers
+console.log(num1-num2);
+
+// Task 3: Multiply two Numbers
+console.log(num1*num2);
+
+// Task 4: Divide two Numbers
+console.log(num1/num2);
+
+// Task 5: Find remainder
+console.log(num1%num2);
+
+// Task 6: Use of += to add a number to a variable
+let add = 10;
+console.log(add += 5);
+
+// Task 7: Use of -= to subtract a number to a variable
+let sub = 50;
+console.log(sub -= 5);
+
+// Task 8: Use of '<' and '>' to add a number to a variable
+console.log(10 < 100);
+console.log(10 > 100);
+
+// Task 9: Use of '>=' and '<=' to add a number to a variable
+console.log(10 >= 100);
+console.log(10 <= 100);
+
+// Task 10: Use of '==' and '===' to add a number to a variable
+console.log('10' == 10); // true
+console.log('10' === 10); // false (=== checks Type of variable)
+
+let num3 = 10;
+let num4 = 20;
+let num5 = 30;
+let num6 = 40;
+// Task 11: Use of && operator to combine two conditions
+console.log(num4 > num3 && num6 > num5);
+
+// Task 12: Use of || operator to combine two conditions
+console.log(num4 > num3 || num5 > num6);
+
+// Task 13: Use of ! operator to negate a conditions
+console.log(!num3);
+
+// Task 14: Use of ternary operator to check a number +ve or -ve
+console.log(num3 > 0 ? 'Positive' : 'Negative')
+
+
+
